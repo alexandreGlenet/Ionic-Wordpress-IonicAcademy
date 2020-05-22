c8a7cf6a65dbc075023a5cb2105864f479a07417
Alexandre Glenet <alexandreglenet30@gmail.com>
2020-05-22 14:07:29 +0200

    Account page.ts part1

--- src/app/pages/account/account.page.ts
+++ src/app/pages/account/account.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-account',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountPage implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup;
+
+  constructor(
+    private api: ApiService, 
+    private fb: FormBuilder,
+    private alertCtrl: AlertController) { }
 
   ngOnInit() {
+    this.userForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
   }
+
+  login() {
+    this.api.signIn(this.userForm.value.username, this.userForm.value.password).subscribe(
+      res => {
+        console.log('login finished');
+      },
+      async err => {
+        const alert = await this.alertCtrl.create({
+          header: err.error.code,
+          subHeader: err.error.data.status,
+          message: err.error.message,
+          buttons: ['OK']
+        });
+        await alert.present();
+      }
+    );
+  }
+
+  
 
 }
