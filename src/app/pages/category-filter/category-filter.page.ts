043567bc4125ef133090fdfebbc5a620fbd6af9e
Alexandre Glenet <alexandreglenet30@gmail.com>
2020-05-22 11:16:11 +0200

    suite

--- src/app/pages/category-filter/category-filter.page.ts
+++ src/app/pages/category-filter/category-filter.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
+import { PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-category-filter',
@@ -9,14 +10,20 @@ import { ApiService } from 'src/app/services/api.service';
 export class CategoryFilterPage implements OnInit {
 
   categories = [];
+  selected = null;
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private popoverCtrl: PopoverController) { }
 
   ngOnInit() {
     this.api.getCategories().subscribe(res => {
       console.log('cat :', res);
       this.categories = res;
+      this.categories.unshift({ name: 'All', id: null });
     });
+  }
+
+  selectCat(cat) {
+    this.popoverCtrl.dismiss(cat);
   }
 
 }
